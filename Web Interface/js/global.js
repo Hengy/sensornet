0b2229db5063fbdd35b6d3d3d0da3ea88184b46f
Matt Hengeveld <heng7500@mylaurier.ca>
2014-09-11 16:02:06 -0400

    Upload Website
    Uploaded started website concept.

---
+++ Web Interface/js/global.js
@@ -0,0 +1,9 @@
+var images = new Array();
+
+function preload() {
+	for (i = 0; i < preload.arguments.length; i++) {
+		images[i] = new Image();
+		images[i].src = preload.arguments[i];
+	}
+	//console.log("Images loaded.");
+}
