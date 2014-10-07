2340473df85a054656325404ecf3f610b43baa1a
Matt Hengeveld <heng7500@mylaurier.ca>
2014-10-07 09:24:19 -0400

    Arduino nRFSN library

--- Visual Studio/nRFSNlib/nRFSNlib/nRFSN.cpp
+++ Visual Studio/nRFSNlib/nRFSNlib/nRFSN.cpp
@@ -1,12 +1,32 @@
+#include "nRF24L01+.h"
 #include "nRFSN.h"
 
-void nRFSN::init(uint8_t SPIFreq, uint8_t CEpin, uint8_t CSNpin, uint8_t IRQpin, uint8_t intNum)
+void nRFSN::init(uint8_t SPIDiv, uint8_t CEpin, uint8_t CSNpin, uint8_t IRQpin, uint8_t intNum)
 {
-	const uint8_t nRFSN_CE = CEpin;
-	const uint8_t nRFSN_CSN = CSNpin;
-	const uint8_t nRFSN_IRQ = IRQpin;
+	nRFSN_CE = CEpin;
+	nRFSN_CSN = CSNpin;
+	nRFSN_IRQ = IRQpin;
 
-	attachInterrupt(intNum,RX_ISR,LOW);
+	pinMode(nRFSN_CE, OUTPUT);
+	pinMode(nRFSN_CE, OUTPUT);
+	pinMode(nRFSN_IRQ, INPUT);
+	digitalWrite(nRFSN_CE, HIGH);
+	digitalWrite(nRFSN_CSN, HIGH);
+
+	initSPI(SPIDiv);
+
+	CONFIG_CURR           = B00101011;   // Show RX_DR and MAX_RT interrupts; Enable CRC - 1 uint8_t; Power up; RX
+	EN_AA_CURR            = B00000011;   // Enable Auto Ack for pipe 0,1
+	EN_RXADDR_CURR        = B00000011;   // Enable data pipe 0,1
+	SETUP_AW_CURR         = B00000010;   // Set up for 4 address
+	SETUP_RETR_CURR       = B00100000;   // 750us retransmit delay; Disable auto retransmit
+	RF_CH_CURR            = B01101001;   // Channel 105 (2.400GHz + 0.105GHz = 2.505GHz)
+	RF_SETUP_CURR         = B00000110;   // RF data rate to 1Mbps; 0dBm output power (highest)
+	RX_PW_P0_CURR         = B00000001;   // 1 payload
+	DYNPD_CURR            = B00000011;   // Set dynamic payload for pipe 0
+	FEATURE_CURR          = B00000100;   // Enable dynamic payload
+	RX_ADDRESS[4]         = {0xC7,0xC7,0xC7,0xC7};
+	TX_ADDRESS[4]         = {0xC7,0xC7,0xC7,0xC7};
 
 	// nRF24L01+ setup
 	// Write to CONFIG register
@@ -35,11 +55,14 @@ void nRFSN::init(uint8_t SPIFreq, uint8_t CEpin, uint8_t CSNpin, uint8_t IRQpin,
 	spiTransfer('n',FLUSH_RX,0);
 	// Flush TX FIFO
 	spiTransfer('n',FLUSH_TX,0);
+
+	nRFSN_RXInt = 0;
+	attachInterrupt(intNum,RX_ISR,LOW);
 }
 
 void nRFSN::RX_ISR(void)
 {
-	intRXData++;
+	nRFSN_RXInt = 1;
 }
 
 
@@ -97,4 +120,24 @@ void nRFSN::setTXAddr(uint8_t addr[], uint8_t len)
 
 void nRFSN::setRXAddr(uint8_t pipe, uint8_t addr[], uint8_t len)
 {
+}
+
+
+void nRFSN::initSPI(uint8_t SPIDiv)
+{
+	// SPI setup
+	SPI.setBitOrder(MSBFIRST);              // Set most significant bit first
+	SPI.setClockDivider(SPIDiv);			// Clock to Fosc/SPIDiv
+	SPI.setDataMode(SPI_MODE0);             // Clock polarity 0; clock phase 0
+	SPI.begin();                            // Start SPI
+}
+
+
+void nRFSN::updateStatus(void)
+{
+	digitalWrite(nRFSN_CSN, LOW);
+
+	nRFSN_Status = SPI.transfer(NRF_NOP);
+
+	digitalWrite(nRFSN_CSN, HIGH);
 }
--- Visual Studio/nRFSNlib/nRFSNlib/nRFSN.h
+++ Visual Studio/nRFSNlib/nRFSNlib/nRFSN.h
@@ -2,21 +2,13 @@
 #include <SPI.h>
 #endif
 
-#include "nRF24L01+.h"
 #include <stdint.h>
 
 class nRFSN
 {
 
-	uint8_t dataBufIn[32];		// 32 uint8_t buffer for all incoming SPI data
-	uint8_t dataBufOut[32];		// 32 uint8_t buffer for all outgoing SPI data
-
-	uint8_t nrfSTATUS;			// nRF24L01+ STATUS register
-
-	volatile int intRXData;     // nRF received packet flag; set by ISR
-
 public:
-	void init(uint8_t SPIFreq, uint8_t CEpin, uint8_t CSNpin, uint8_t IRQpin, uint8_t intNum);
+	void init(uint8_t SPIDiv, uint8_t CEpin, uint8_t CSNpin, uint8_t IRQpin, uint8_t intNum);
 	void RX_ISR(void);
 	void setPower(uint8_t pwrLvl);
 	void setTXMode(void);
@@ -26,25 +18,39 @@ public:
 	uint8_t transmit(void);
 	uint8_t getPayloadSize(void);
 	uint8_t getPayload(void);
+	void initSPI(uint8_t SPIDiv);
+	void updateStatus(void);
+
+	uint8_t nRFSN_CE;
+	uint8_t nRFSN_CSN;
+	uint8_t nRFSN_IRQ;
+
+	uint8_t nRFSN_BufIn[32];		// 32 uint8_t buffer for all incoming SPI data
+	uint8_t nRFSN_BufOut[32];		// 32 uint8_t buffer for all outgoing SPI data
+
+	uint8_t nRFSN_Status;			// nRF24L01+ STATUS register
+
+	volatile int nRFSN_RXInt;     // nRF received packet flag; set by ISR
+
 protected:
 	void configReg(char wr, uint8_t command, uint8_t data);
 	void setTXAddr(uint8_t addr[], uint8_t len);
 	void setRXAddr(uint8_t pipe, uint8_t addr[], uint8_t len);
 	
 	/*------------------------------------------------
-	 * nRF24L01+ current config settings
+	 * nRF24L01+ config variables
 	------------------------------------------------*/
-	uint8_t CONFIG_CURR           = B00101011;   // Show RX_DR and MAX_RT interrupts; Enable CRC - 1 uint8_t; Power up; RX
-	uint8_t EN_AA_CURR            = B00000011;   // Enable Auto Ack for pipe 0,1
-	uint8_t EN_RXADDR_CURR        = B00000011;   // Enable data pipe 0,1
-	uint8_t SETUP_AW_CURR         = B00000010;   // Set up for 4 uint8_t address
-	uint8_t SETUP_RETR_CURR       = B00100000;   // 750us retransmit delay; Disable auto retransmit
-	uint8_t RF_CH_CURR            = B01101001;   // Channel 105 (2.400GHz + 0.105GHz = 2.505GHz)
-	uint8_t RF_SETUP_CURR         = B00000110;   // RF data rate to 1Mbps; 0dBm output power (highest)
-	uint8_t RX_PW_P0_CURR         = B00000001;   // 1 uint8_t payload
-	uint8_t DYNPD_CURR            = B00000011;   // Set dynamic payload for pipe 0
-	uint8_t FEATURE_CURR          = B00000100;   // Enable dynamic payload
-	uint8_t RX_ADDRESS[4]         = {0xC7,0xC7,0xC7,0xC7};
-	uint8_t TX_ADDRESS[4]         = {0xC7,0xC7,0xC7,0xC7};
+	uint8_t CONFIG_CURR;
+	uint8_t EN_AA_CURR;
+	uint8_t EN_RXADDR_CURR;
+	uint8_t SETUP_AW_CURR;
+	uint8_t SETUP_RETR_CURR;
+	uint8_t RF_CH_CURR;
+	uint8_t RF_SETUP_CURR;
+	uint8_t RX_PW_P0_CURR;
+	uint8_t DYNPD_CURR;
+	uint8_t FEATURE_CURR;
+	uint8_t RX_ADDRESS[4];
+	uint8_t TX_ADDRESS[4];
 };
 
