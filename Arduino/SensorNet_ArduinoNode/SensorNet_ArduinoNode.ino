c2e69168e1921a44d835e077a58d8ad6ca8f6686
Matt Hengeveld <heng7500@mylaurier.ca>
2014-10-14 13:08:12 -0400

    Arduino nRFSN Library

--- Arduino/SensorNet_ArduinoNode/SensorNet_ArduinoNode.ino
+++ Arduino/SensorNet_ArduinoNode/SensorNet_ArduinoNode.ino
@@ -1,9 +1,11 @@
+#include <nRFSN.h>
+
 void setup() {
   // put your setup code here, to run once:
-
+  nRFSN.init(SPI_CLOCK_DIV4,5,4,3,0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  
+  nRFSN.setRXMode();
 }
