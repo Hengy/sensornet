55e95be01b49f08ffbc5bdb06ce540d9e44b76fb
Matt Hengeveld <heng7500@mylaurier.ca>
2014-10-16 12:33:04 -0400

    PIC nRFSN Library
    PIC library now .c file
    compiles using #include <nRFSN/nRFSN.h>
    and folder in project properties "include directories"

---
+++ MPLAB X/SensorNet_Temp.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Student
+ *
+ * Created on October 16, 2014, 12:04 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <nRFSN/nRFSN.h>
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
