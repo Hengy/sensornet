8caa812bff5d90c22f60521cf0939b8a1180bb17
Matt Hengeveld <heng7500@mylaurier.ca>
2014-10-17 11:08:32 -0400

    Library stuff

---
+++ MPLAB X_OLD/nRFSN_testcpp.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=nRFSN_testcpp.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/nRFSN_testcpp.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=nrfsntestcpp.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/nrfsntestcpp.x.tar
