a6eaa3b4d8ba494401873ddb6ab59a95cb0e4a74
Matt Hengeveld <heng7500@mylaurier.ca>
2014-11-14 12:11:02 -0500

    RPi nRFSN library - MAX_RT interrupt tripping

--- Arduino/nRF_RX/nRF_RX.ino
+++ Arduino/nRF_RX/nRF_RX.ino
@@ -102,7 +102,6 @@ byte SETUP_AW_CURR         = B00000010;   // Set up for 4 byte address
 byte SETUP_RETR_CURR       = B00100000;   // 750us retransmit delay; Disable auto retransmit
 byte RF_CH_CURR            = B01101001;   // Channel 105 (2.400GHz + 0.105GHz = 2.505GHz)
 byte RF_SETUP_CURR         = B00000110;   // RF data rate to 1Mbps; 0dBm output power (highest)
-//byte RF_SETUP_CURR         = B00001110;   // RF data rate to 2Mbps; 0dBm output power (highest)
 byte RX_PW_P0_CURR         = B00000001;   // 1 byte payload
 byte DYNPD_CURR            = B00000011;   // Set dynamic payload for pipe 0
 byte FEATURE_CURR          = B00000100;   // Enable dynamic payload
